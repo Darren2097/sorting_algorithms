7d423d27b451e24aa80f195ecf5b8ae6afa7968b
Darren <darren2097@gmail.com>
2022-10-07 16:27:09 +0200

    Quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -17,7 +17,7 @@ int partition(int *array, size_t size, int left, int right)
 	int j = 0;
 	int temp = 0;
 
-	for (j = left; j <= right - 1; j++)
+	for (j = left; j <= right -1; j++)
 	{
 		if (array[i] < pivot)
 		{
